4a730ebc3b787144d9451a09a74c0b0908e8c545
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-06-03 02:10:45 -0700

    integrated both recyclerView

--- app/src/main/java/com/hanseltritama/multirecyclerview/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/multirecyclerview/MainActivity.kt
@@ -3,17 +3,44 @@ package com.hanseltritama.multirecyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.hanseltritama.multirecyclerview.adapters.VerticalAdapter
+import com.hanseltritama.multirecyclerview.model.HorizontalModel
+import com.hanseltritama.multirecyclerview.model.VerticalModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: VerticalAdapter
+    lateinit var arrayListVertical: ArrayList<VerticalModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arrayListVertical = ArrayList()
+
         rv_main_activity.setHasFixedSize(true)
         rv_main_activity.layoutManager = LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL,
                 false)
 
+        adapter = VerticalAdapter(this, arrayListVertical)
 
+        rv_main_activity.adapter = adapter
+
+        setData()
+    }
+
+    private fun setData() {
+        val arrayListHorizontal: ArrayList<HorizontalModel> = ArrayList()
+        for (i in 0..5) {
+            for (j in 0..5) {
+                val horizontalModel = HorizontalModel("Name: $j","Description: $j")
+                arrayListHorizontal.add(horizontalModel)
+            }
+            val verticalModel = VerticalModel("Title $i", arrayListHorizontal)
+            arrayListVertical.add(verticalModel)
+        }
+        adapter.notifyDataSetChanged()
     }
 }
